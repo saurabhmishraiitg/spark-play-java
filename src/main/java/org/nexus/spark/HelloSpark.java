178b60135187e5f527db4d6b23748ffe2dcede75
Saurabh Mishra <35100561+saurabhmishraiitg@users.noreply.github.com>
2018-11-03 21:09:00 +0530

    [UPD] Added sample println

--- src/main/java/org/nexus/spark/HelloSpark.java
+++ src/main/java/org/nexus/spark/HelloSpark.java
@@ -4,6 +4,7 @@ public class HelloSpark {
     public static void main(String[] args) {
         System.out.println("Sparking");
         System.out.println("Sparking1");
+        System.out.println("Sparking2");
     }
 }
 
