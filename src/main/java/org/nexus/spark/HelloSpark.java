b7418a3b28f5f477210ef5b75a767f8713e2abae
Saurabh Mishra <35100561+saurabhmishraiitg@users.noreply.github.com>
2018-11-03 21:10:22 +0530

    [UPD] Removed redundant println

--- src/main/java/org/nexus/spark/HelloSpark.java
+++ src/main/java/org/nexus/spark/HelloSpark.java
@@ -3,8 +3,6 @@ package org.nexus.spark;
 public class HelloSpark {
     public static void main(String[] args) {
         System.out.println("Sparking");
-        System.out.println("Sparking1");
-        System.out.println("Sparking2");
     }
 }
 
